0416cb4c8ee3aa20a68cdf92925fda1e750d22ab
Pavitra Golchha <hello@pavi2410.me>
2022-03-30 00:15:39 +0530

    download: add footer

--- src/Download.jsx
+++ src/Download.jsx
@@ -58,6 +58,14 @@ export default function Download() {
           </Button>
         </Box>
       </Center>
+      <Footer height={60}>
+        <Group position="apart" sx={{ height: '100%' }} px={20}>
+          <Text>Built by <Anchor href="https://pavi2410.me" target="_blank">
+            pavi2410
+          </Anchor></Text>
+          <Button variant="subtle" component={Anchor} href="https://github.com/pavi2410/sendy" target="_blank">Sauce</Button>
+        </Group>
+      </Footer>
     </Container>
   )
 }
