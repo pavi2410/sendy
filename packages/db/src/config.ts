83b05c4ee5a1bb2b988abb4af86bf8d3c891238c
Pavitra Golchha <hello@pavi2410.com>
2026-02-12 21:29:10 +0530

    bump max upload limit to 150MB

--- packages/db/src/config.ts
+++ packages/db/src/config.ts
@@ -2,7 +2,7 @@
 export const DEFAULT_EXPIRATION_DAYS = 7;
 
 /** Maximum file size in MB */
-export const MAX_FILE_SIZE_MB = 50;
+export const MAX_FILE_SIZE_MB = 150;
 
 /** Maximum file size in bytes */
 export const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024;
